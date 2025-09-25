f2df9452fcfee3cd046338633d8ddcb9eaecb13f
ronak <ronakpraja340@gmail.com>
2025-09-25 22:01:22 +0530

    content and data changes

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -83,47 +83,4 @@ export const speakers = [
         "id": "18",
         "national": true
     },
-    // {
-    //     "name": "Dr. Laura Sanita",
-    //     "designation": "Associate Professor in the Department of Computing Sciences of Bocconi",
-    //     "image": "/gallery/keynote_speakers/Sanita.jpg",
-    //     "id": "1",
-    //     "national": false
-    // },
-    // {
-    //     "name": "Dr. Maria Ganzha",
-    //     "designation": "Associate Professor",
-    //     "image": "/gallery/speial_sessions_old/Aarti.jpg",
-    //     "id": "2",
-    //     "national": true
-    // },
-    // {
-    //     "name": "Dr. Nasib Singh Gill",
-    //     "designation": "Associate Professor",
-    //     "image": "/galleryspecial_sessions_old/Ashima.jpg",
-    //     "id": "3",
-    //     "national": true
-    // },
-    
-    // {
-    //     "name": "Dr. J. C. Bansal",
-    //     "designation": "Associate Professor",
-    //     "image": "/gallery/special_sessions_old/Hari_Mohan.png",
-    //     "id": "1",
-    //     "national": true
-    // },
-    // {
-    //     "name": "Dr. Maria Ganzha",
-    //     "designation": "Associate Professor",
-    //     "image": "/gallery/speial_sessions_old/Aarti.jpg",
-    //     "id": "2",
-    //     "national": true
-    // },
-    // {
-    //     "name": "Dr. Nasib Singh Gill",
-    //     "designation": "Associate Professor",
-    //     "image": "/galleryspecial_sessions_old/Ashima.jpg",
-    //     "id": "3",
-    //     "national": true
-    // },
 ]
